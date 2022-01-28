6f31e258cdb4c117e339c74791abe7b7482d9b7a
Louise <louisegreen18@gmail.com>
2022-01-28 14:19:12 +0000

    test schema added
    test schema added, sql file

---
+++ code/my_test_schema.sql
@@ -0,0 +1,16 @@
+create schema `my_test_schema`;
+
+create table `my_test_schema.people`(
+`given_name` varchar(80) default null,
+`family_name` varchar(80) default null,
+`date_of_birth` int not null auto_increment,
+`place_of_birth` int not null auto_increment,
+foreign key (`place_of_birth`),
+);
+
+create table `my_test_schema.places`(
+`city` varchar(80) default null,
+`county` varchar(80) default null,
+`country` varchar(80) default null,
+foreign key (`city`)
+);
